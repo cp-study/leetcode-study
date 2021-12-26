9b2c370709f8f7267cad933c819c1320d9c459ad
SoF <wdj930729@gmail.com>
2021-12-26 12:11:11 +0900

    Create LC_312.py

---
+++ LC_312.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxCoins(self, nums: List[int]) -> int:
+        nums = [1] + nums + [1]
+        n = len(nums)
+        dp = [[0] * n for _ in range(n)]
+    
+        for gap in range(2, n):
+            for i in range(n-gap):
+                j = i + gap
+                for k in range(i+1, j):
+                    dp[i][j] = max(dp[i][j], nums[i] * nums[k] * nums[j] + dp[i][k] + dp[k][j])
+        return dp[0][n-1]
+        
