a9426c6f288c7d317e9c68e7fff7a186b4b11374
jun <murane@naver.com>
2022-01-02 14:49:55 +0900

    Create LC_818.py

---
+++ LC_818.py
@@ -0,0 +1,16 @@
+from collections import deque
+class Solution:
+    def racecar(self, target: int) -> int:
+        
+        queue = deque([(0, 0, 1)])
+        while queue:
+            
+            moves, pos, vel = queue.popleft()
+
+            if pos == target:
+                return moves
+            
+            queue.append((moves + 1, pos + vel, 2 * vel))
+            
+            if (pos + vel > target and vel > 0) or (pos + vel < target and vel < 0):
+                queue.append((moves + 1, pos, -vel / abs(vel)))
