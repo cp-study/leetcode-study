fcdc7f1cbe05fa110a84e9c6b9e72d0689cd306f
SoF <wdj930729@gmail.com>
2022-07-03 14:13:00 +0900

    Create LC_1000.py

---
+++ LC_1000.py
@@ -0,0 +1,21 @@
+class Solution:
+    def mergeStones(self, stones, K):
+        n = len(stones)
+        inf = float('inf')
+        prefix = [0] * (n + 1)
+        for i in range(n):
+            prefix[i + 1] = prefix[i] + stones[i]
+
+        import functools
+
+        @functools.lru_cache(None)
+        def dp(i, j, m):
+            if (j - i + 1 - m) % (K - 1):
+                return inf
+            if i == j:
+                return 0 if m == 1 else inf
+            if m == 1:
+                return dp(i, j, K) + prefix[j + 1] - prefix[i]
+            return min(dp(i, mid, 1) + dp(mid + 1, j, m - 1) for mid in range(i, j, K - 1))
+        res = dp(0, n - 1, 1)
+        return res if res < inf else -1
